2f00eb25492230b015ddc140d9741e467433a64c
AndersonAPinto <andersonaugustopinto@gmail.com>
2022-06-15 06:50:39 -0300

    'Criado método cumprimentar'

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,11 @@
 class Pessoa:
-    def cumprimentar(self):
+    def cumprimentar(self):  # método pertence sempre a uma class.
+        # self, significa que o método esta sempre chamando o OBJETO.
         return f'Olá Mundo! {id(self)}'
+
 
 if __name__ == '__main__':
     p = Pessoa()
     print(Pessoa.cumprimentar(p))
     print(id(p))
-    print(p.cumprimentar())
+    print(p.cumprimentar())  # self já declara o objeto como primeiro parâmetro.
